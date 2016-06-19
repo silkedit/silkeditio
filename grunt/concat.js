c70a64e67e6c47c40026086ffac458525b14f8f6
mizuki-y <mizuki-y@syati.info>
2016-06-19 17:43:28 +0900

    Add download section on release page

--- grunt/concat.js
+++ grunt/concat.js
@@ -3,10 +3,12 @@ module.exports = {
   default: {
     options: {
       banner: "<% layout('../layout/base') -%>\n" +
-      "<% block('title', 'Release -') -%>\n\n"
+        "<% block('title', 'Release -') -%>\n\n" +
+        "<%- partial('partials/releaseHead') %>\n\n"
     },
     src: (function(){
       const files = fs.readdirSync(__dirname + '/../src/post/release');
+
       // ファイルの並びを逆順にする
       return files.reverse().map(function(file){
         return 'src/post/release/' + file;
